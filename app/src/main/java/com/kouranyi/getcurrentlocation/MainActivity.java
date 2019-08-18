5906d75eee90518154cec58e89f22fc01422476b
kouranyi <elkouranyi@gmail.com>
2019-08-18 03:49:51 +0200

    Creating the FusedLocationProviderClient

--- app/src/main/java/com/kouranyi/getcurrentlocation/MainActivity.java
+++ app/src/main/java/com/kouranyi/getcurrentlocation/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
 public class MainActivity extends AppCompatActivity {
 
+    private FusedLocationProviderClient mFusedLocationClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createFusedLocationProviderClient();
+    }
+
+
+    private void createFusedLocationProviderClient() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
 }
