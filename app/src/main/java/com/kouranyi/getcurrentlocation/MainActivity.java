9f18cd416a1eb7564d2fc4e8238279120e0478eb
kouranyi <elkouranyi@gmail.com>
2019-08-18 03:56:16 +0200

    Creating the LocationRequest

--- app/src/main/java/com/kouranyi/getcurrentlocation/MainActivity.java
+++ app/src/main/java/com/kouranyi/getcurrentlocation/MainActivity.java
@@ -5,21 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity {
 
     private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest mLocationRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         createFusedLocationProviderClient();
+        createLocationRequest();
     }
 
 
     private void createFusedLocationProviderClient() {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+    private void createLocationRequest() {
+
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(10*000); //get updated about location changes every 5 seconds
+        mLocationRequest.setFastestInterval(5000); // update rate upper limit is 5 seconds
     }
 }
